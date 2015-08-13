8cefa498604ad04c3f39713431dfcff7eba95165
Connor <avaterq0@gmail.com>
2015-08-13 16:08:53 -0700

    Create GameObject.java

---
+++ GameObject.java
@@ -0,0 +1,53 @@
+package marshmellow;
+
+import java.awt.Graphics;
+
+//information used by all objects; this is extended
+//help handler know how to handle it
+public abstract class GameObject {
+
+	protected int x, y;
+	protected ID id;
+	protected int velX, velY;
+	
+	public GameObject(int x, int y, ID id){
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+	
+	public abstract void tick(); // this deals with tick
+	public abstract void render(Graphics g); // this renders created object
+
+//getters and setters
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public ID getId() {
+		return id;
+	}
+	public void setId(ID id) {
+		this.id = id;
+	}
+	public int getVelX() {
+		return velX;
+	}
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	public int getVelY() {
+		return velY;
+	}
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+}
