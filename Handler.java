fcc4ad0731d1c1bcf05347ef115531f7ad259683
Connor <avaterq0@gmail.com>
2015-08-13 16:08:33 -0700

    Create Handler.java

---
+++ Handler.java
@@ -0,0 +1,35 @@
+package marshmellow;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+//handles objects created
+//loops through, updates and renders them
+public class Handler { 
+	
+	LinkedList<GameObject> objectList = new LinkedList<GameObject>();
+	
+	public void tick(){
+		for(int i = 0; i < objectList.size(); i++){
+			GameObject tempObject = objectList.get(i);
+			
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g){
+		for(int i = 0; i < objectList.size(); i++){
+			GameObject tempObject = objectList.get(i);
+			
+			tempObject.render(g);
+		}
+	}
+	
+	public void addObject(GameObject object){
+		this.objectList.add(object);
+	}
+	
+	public void removeObject(GameObject object){
+		this.objectList.remove(object);
+	}
+}
